6510d69523ce8c283d1622c3f0aacff1ba9132be
rlaqkq48 <rlaqkq488@gmail.com>
2024-08-25 17:03:23 +0900

    히히

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -6,7 +6,10 @@ public class PlayerMotor : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 playerVelocity;
-    private float speed = 5f;
+    private bool IsGrounded;
+    public float speed = 5f;
+    public float gravity = -9.8f;
+    public float jumpHeight = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +19,7 @@ public class PlayerMotor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        IsGrounded = controller.isGrounded;
     }
     public void ProcessMove(Vector2 input)
     {
@@ -24,5 +27,17 @@ public class PlayerMotor : MonoBehaviour
         moveDirection.x = input.x;
         moveDirection.z = input.y;
         controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
+        playerVelocity.y += gravity * Time.deltaTime;
+        if (IsGrounded && playerVelocity.y < 0)
+            playerVelocity.y = -2f;
+        controller.Move(playerVelocity * Time.deltaTime);
+        Debug.Log(playerVelocity.y);
+    }
+    public void Jump()
+    {
+        if (IsGrounded)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        }
     }
 }
