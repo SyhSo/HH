3c18b56107870c1a22832a43be9f520f3b1a30ee
SyhSo <yj1862392@gmail.com>
2024-10-19 23:32:50 +0900

    총 사운드 추가

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -1,17 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
+using static Unity.VisualScripting.Member;
+
+[RequireComponent(typeof(AudioSource))]
 
 public class Fire : MonoBehaviour
 {
+    //총알 프리팹
     public GameObject bullet;
+    //총알 발사좌표
     public Transform FirePos;
-    // Update is called once per frame
+    //총알 발사 사운드
+    public AudioClip fireSfx;
+    //AudioSource 컴포넌트 저장 변수
+    public AudioSource source = null;
+
+    void Start()
+    {
+        //AudioSource 컴포넌트 추출, 변수 할당
+        source = GetComponent<AudioSource>();
+    }
     void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
             Instantiate(bullet, FirePos.transform.position, FirePos.transform.rotation);
+
+            source.PlayOneShot(fireSfx, 0.9f);
         }
     }
 }
